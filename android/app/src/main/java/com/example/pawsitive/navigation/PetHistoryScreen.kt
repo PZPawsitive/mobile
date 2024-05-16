2ff6b519481db0549338ca9868c0062e7169ea80
michalswistowski <michalswistowski2002@gmail.com>
2024-05-16 09:30:46 +0200

    added removing walk history on history screen

--- android/app/src/main/java/com/example/pawsitive/navigation/PetHistoryScreen.kt
+++ android/app/src/main/java/com/example/pawsitive/navigation/PetHistoryScreen.kt
@@ -1,7 +1,10 @@
 package com.example.pawsitive.navigation
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,21 +14,25 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.osmdroid.util.GeoPoint
 import java.util.Date
 
 data class History(val description: String, val date: Date, val geopoints: List<GeoPoint>)
 
-val histories = listOf(
+val histories = mutableStateListOf(
     History("super spacer", Date(), listOf(GeoPoint(1, 1), GeoPoint(2, 2))),
     History("lipny spacer", Date(), listOf(GeoPoint(1, 1))),
     History("walka psów", Date(), listOf(GeoPoint(1, 1))),
@@ -33,45 +40,64 @@ val histories = listOf(
     History("piesek był zadowolony", Date(), listOf(GeoPoint(1, 1))),
 )
 
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun PetHistoryScreen(showHistoryMap: () -> Unit) {
-    Column {
-        Text(text = "Historia spacerów")
-    }
-    LazyColumn(modifier = Modifier.padding(10.dp)) {
-        items(items = histories) {
-            var expandedSettings by remember {
-                mutableStateOf(false)
-            }
-            Box() {
-                DropdownMenu( // ui broken - fix
-                    expanded = expandedSettings,
-                    onDismissRequest = { expandedSettings = false }
-                ) {
-                    DropdownMenuItem(
-                        text = { Text(text = "Zmień opis") },
-                        onClick = { TODO() })
-                    Divider()
-                    DropdownMenuItem(text = { Text(text = "Usuń spacer") }, onClick = { TODO() })
-                    Divider()
-                    DropdownMenuItem(
-                        text = { Text(text = "Zobacz na mapie") },
-                        onClick = { showHistoryMap() })
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Text(
+            text = "Historia spacerów",
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxSize()
+        ) {
+            items(items = histories) {
+                var expandedSettings by remember {
+                    mutableStateOf(false)
                 }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp),
-                    onClick = { expandedSettings = !expandedSettings }
-                ) {
-                    Column(modifier = Modifier.padding(5.dp)) {
-                        Text(text = it.description, fontWeight = FontWeight.Bold)
-                        Text(text = it.date.toString())
+                Box {
+                    DropdownMenu( // ui broken - fix
+                        expanded = expandedSettings,
+                        onDismissRequest = { expandedSettings = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text(text = "Zmień opis") },
+                            onClick = { TODO() })
+                        HorizontalDivider()
+                        DropdownMenuItem(text = { Text(text = "Usuń spacer") }, onClick = {
+                            expandedSettings = !expandedSettings
+                            histories.remove(it)
+                        })
+                        HorizontalDivider()
+                        DropdownMenuItem(
+                            text = { Text(text = "Zobacz na mapie") },
+                            onClick = { showHistoryMap() })
+                    }
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        onClick = { expandedSettings = !expandedSettings }
+                    ) {
+                        Column(modifier = Modifier.padding(5.dp)) {
+                            Text(text = it.description, fontWeight = FontWeight.Bold)
+                            Text(text = it.date.toString())
+                        }
                     }
                 }
-            }
 
+            }
         }
     }
+
 }
